3e80eb5ceaddd3bf1640a51a5c26937618cf6b29
pavo1hr <ivan96jp@gmail.com>
2019-05-03 12:13:11 +0200

    added tests

---
+++ src/test/java/SolverTest.java
@@ -0,0 +1,58 @@
+import ch.supsi.tsp_algoritmi.Main;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolverTest {
+
+    @Test(timeout = 181000)
+    public void ch130() {
+        Main.start("ch130", 1556695909517L);
+    }
+
+    @Test(timeout = 181000)
+    public void d198() {
+        Main.start("d198", 1556695043501L);
+    }
+
+    @Test(timeout = 181000)
+    public void eil76() {
+        Main.start("eil76", 1556698623315L);
+    }
+
+    @Test(timeout = 181000)
+    public void fl1577() {
+        Main.start("fl1577", 1556463618140L);
+    }
+
+    @Test(timeout = 181000)
+    public void kroA100() {
+        Main.start("kroA100", 1556696542522L);
+    }
+
+    @Test(timeout = 181000)
+    public void lin318() {
+        Main.start("lin318", 1556637004972L);
+    }
+
+    @Test(timeout = 181000)
+    public void pcb442() {
+        Main.start("pcb442", 1556615082428L);
+    }
+
+    @Test(timeout = 181000)
+    public void pr439() {
+        Main.start("pr439", 1556610643066L);
+    }
+
+    @Test(timeout = 181000)
+    public void rat783() {
+        Main.start("rat783", 1556398060866L);
+    }
+
+    @Test(timeout = 181000)
+    public void u1060() {
+        Main.start("u1060", 1556534722481L);
+    }
+}
+
